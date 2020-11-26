07fd2a19e7799a30bd3a0d0baf3cd5279dc4ec33
ikhvichi <irina.khvichiya@t-systems.com>
2020-11-26 12:06:24 +0100

    Задание 7 - done

--- java-example/src/test/java/ru/selenium/training/Task7_waits.java
+++ java-example/src/test/java/ru/selenium/training/Task7_waits.java
@@ -5,14 +5,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 /**
@@ -20,8 +20,7 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
  * Сценарий должен выполнять следующие действия:
  * 1) входит в панель администратора http://localhost/litecart/admin
  * 2) прокликивает последовательно все пункты меню слева, включая вложенные пункты
- * 3) для каждой страницы проверяет наличие заголовка (то есть элемента с
- * тегом h1)
+ * 3) для каждой страницы проверяет наличие заголовка
  */
 public class Task7_waits {
     private WebDriver driver;
@@ -31,76 +30,79 @@ public class Task7_waits {
     public void start() {
         //System.setProperty("webdriver.chrome.driver", "C:\\workspace\\chromedriver.exe");
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
     }
 
     @Test
-    public void clickEveryLeftMenuOption() {
+    public void clickEveryLeftMenuOption() throws InterruptedException {
+        //login
         loginToAdminPageAsAdmin();
-        //Check that user is logged in on the right web page
-        List<WebElement> rows = driver.findElements(By.xpath("//ul[@id='box-apps-menu']//li[contains(@class,'app')]/a"));
+        //count main options
+        assertTrue(areElementsPresent("//ul[@id='box-apps-menu']//li[contains(@class,'app')]/a"));
+        List<WebElement> rows = wait.until(d -> d.findElements(By.xpath("//ul[@id='box-apps-menu']//li[contains(@class,'app')]/a")));
         final int initialSize = rows.size();
         for (int i = 0; i < initialSize; i++) {
-            List<WebElement> rows2 = driver.findElements(By.xpath("//ul[@id='box-apps-menu']//li[contains(@class,'app')]/a"));
-//                clickMainOption(rows2.get(i));
-//                String foundTitle = driver.getTitle();
-//                wait.until(titleIs(foundTitle));
-            System.out.println("Title is found: " + rows2.get(i).getText());
-            rows2.get(i).click();
-            //     i = i +1;
-//                if (isElementPresent("//li[@class='app selected']/ul[@class='docs']")) {
-//                    List<WebElement> subrows = driver.findElements(By.xpath("//li[@class='app selected']/ul[@class='docs']//li"));
-//                    for (WebElement subrow : subrows) {
-//                        clickSubOption(subrow);
-//                        String foundSubTitle = driver.getTitle();
-//                        wait.until(titleIs(foundTitle));
-//                        System.out.println("Subtitle is found: " + foundSubTitle);
-//                    }
-//                }
+            List<WebElement> rows1 = wait.until(d -> d.findElements(By.xpath("//ul[@id='box-apps-menu']//li[contains(@class,'app')]/a")));
+            System.out.println("Title is found: " + i + ": " + rows1.get(i).getText());
+            rows1.get(i).click();
+            //click suboption(s)
+            clickEverySubOption();
         }
     }
 
-    public void loginToAdminPageAsAdmin() {
+    public void clickEverySubOption() {
+        if (isElementPresent("//ul[@class='docs']")) {
+            List<WebElement> subrows = wait.until(d -> d.findElements(By.xpath("//ul[@class='docs']//span[@class='name']")));
+            final int initialSize2 = subrows.size();
+//                    for (WebElement subrow : subrows) {
+            for (int j = 0; j < initialSize2; j++) {
+                List<WebElement> subrows1 = wait.until(d -> d.findElements(By.xpath("//ul[@class='docs']//span[@class='name']")));
+                System.out.println("Subtitle is found: " + j + ": " + subrows1.get(j).getAttribute("innerText"));
+                subrows1.get(j).click();
+            }
+        }
+    }
+
+    @FindBy(xpath = "//button[@class='btn btn-default']")
+    private WebElement button_login;
+
+    public void loginToAdminPageAsAdmin() throws InterruptedException {
         driver.get("http://localhost:8090/litecart/admin/");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
+        assertTrue(isElementPresent("//button[@class='btn btn-default']"));
+        wait.until(d -> d.findElement(By.xpath("//button[@class='btn btn-default']")));
         driver.findElement(By.xpath("//button[@class='btn btn-default']")).click();
+        System.out.println(driver.getTitle());
+        wait.until(titleIs("Dashboard | My Store"));
     }
 
-    public boolean isTitleCorrect(String text) {
-        String foundText = driver.getTitle();
-        System.out.println(foundText);
-        if (foundText.equals(text)) {
-            if (wait.until(titleIs(text))) {
-                System.out.println("Title is correct: " + text);
-                return true;
-            }
-        } else {
-            System.out.println("Title is incorrect: " + text);
-        }
-        return false;
-    }
-
-    public void clickMainOption(WebElement webElement) {
-        String a = webElement.getText();
-        System.out.println("name of list " + a);
+    public boolean isElementPresent(By locator) {
         try {
-            webElement.click();
-        } catch (StaleElementReferenceException e) {
-            System.out.println("StaleElementException appeared" + e);
+            wait.until(d -> d.findElement(locator));
+            //driver.findElement(locator);
+            return true;
+        } catch (TimeoutException ex) {
+            return false;
         }
     }
 
-    public void clickSubOption(WebElement webElement) {
-        driver.manage().timeouts().setScriptTimeout(3, TimeUnit.SECONDS);
-        driver.manage().timeouts().pageLoadTimeout(3, TimeUnit.SECONDS);
-        try {
-            webElement.click();
-//            waitForAjaxRequests();
-        } catch (StaleElementReferenceException e) {
-            System.out.println("StaleElementException appeared" + e);
-        }
+    public boolean areElementsPresent(By locator) {
+        boolean found;
+        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        found = driver.findElements(locator).size() > 0;
+        driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+        return found;
+    }
+
+    public boolean areElementsPresent(String xpath) {
+        boolean found;
+        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        found = driver.findElements(By.xpath(xpath)).size() > 0;
+        driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+        return found;
     }
 
     public boolean isElementPresent(String xpath) {
