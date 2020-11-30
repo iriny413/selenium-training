a9efd2e6751b316ef2a237a95ec5450c0103d78b
ikhvichi <irina.khvichiya@t-systems.com>
2020-11-30 20:40:12 +0100

    Задание 8 - party done

--- java-example/src/test/java/ru/selenium/training/Task8.java
+++ java-example/src/test/java/ru/selenium/training/Task8.java
@@ -4,14 +4,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.sql.SQLOutput;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
