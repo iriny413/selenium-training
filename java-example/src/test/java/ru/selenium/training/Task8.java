32f22d04593e2a25cdab17c7094befe3588e23e3
ikhvichi <irina.khvichiya@t-systems.com>
2020-11-30 20:39:43 +0100

    Задание 8 - party done

--- java-example/src/test/java/ru/selenium/training/Task8.java
+++ java-example/src/test/java/ru/selenium/training/Task8.java
@@ -13,13 +13,15 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.sql.SQLOutput;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static junit.framework.TestCase.assertTrue;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
-public class Task8 extends CommonMethods{
+public class Task8 extends CommonMethods {
     private WebDriver driver;
     private WebDriverWait wait;
 
@@ -27,27 +29,77 @@ public class Task8 extends CommonMethods{
     public void start() {
         //System.setProperty("webdriver.chrome.driver", "C:\\workspace\\chromedriver.exe");
         driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        //driver.manage().window().maximize();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
     }
 
-    @Test public void checkCountriesSorting(){
+    @Test
+    public void checkCountriesSorting() {
         //login
         loginToAdminPageAsAdmin();
         //open page 'Countries'
         assertTrue(openPage("Countries"));
         List<WebElement> rows = wait.until(d -> d.findElements(By.xpath("//tbody//td[5]/a")));
         final int initialSize = rows.size();
+        List<String> names = new ArrayList<String>();
         for (int i = 0; i < initialSize; i++) {
             List<WebElement> rows1 = wait.until(d -> d.findElements(By.xpath("//tbody//td[5]/a")));
             String countryName = rows1.get(i).getText();
             System.out.println("Country# " + i + " - " + countryName);
+            names.add(countryName);
+            //check Geo zones
+            checkGeoZones(i);
         }
-        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        Collections.sort(names);
+        System.out.println("Alphabetically sorted list of counties is this: " + names);
     }
 
-    public void loginToAdminPageAsAdmin(){
+//    @Test
+//    public void checkCountriesSorting() {
+//        //login
+//        loginToAdminPageAsAdmin();
+//        //open page 'Countries'
+//        assertTrue(openPage("Countries"));
+//        List<WebElement> rows = wait.until(d -> d.findElements(By.xpath("//tbody//td[5]/a")));
+//        final int initialSizeC = rows.size();
+//        System.out.println("Number of countries is " + initialSizeC);
+//        List<String> countryNames = new ArrayList<String>();
+//        for (int i = 0; i < initialSizeC; i++) {
+//            List<WebElement> rows1 = wait.until(d -> d.findElements(By.xpath("//tbody//td[5]/a")));
+//            String countryName = rows1.get(i).getText();
+//            System.out.println("Country # " + i + " - " + countryName);
+//            countryNames.add(countryName);
+//            rows1.get(i).click();
+//
+//            //check Geo zones
+////            checkGeoZones(i);
+//        }
+//        //sort countries names
+//        Collections.sort(countryNames);
+//        System.out.println("Alphabetically sorted list of counties is this: " + countryNames);
+//    }
+
+    public void checkGeoZones(int countryIndex) {
+        if (areElementsPresent("//table[@class='table table-striped table-hover data-table']/tbody/tr")) {
+            List<WebElement> zones = wait.until(d -> d.findElements(By.xpath("//table[@class='table table-striped table-hover data-table']/tbody/tr")));
+            final int initialSizeZ = zones.size();
+            System.out.println("Number of geo zones is " + zones.size());
+            List<String> zoneNames = new ArrayList<String>();
+            for (int j = 0; j < initialSizeZ; j++) {
+                List<WebElement> zones1 = wait.until(d -> d.findElements(By.xpath("//table[@class='table table-striped table-hover data-table']/tbody/tr")));
+                String zoneName = zones1.get(j).getText();
+                System.out.println("Geo zone # " + j + " - " + zoneName);
+                zoneNames.add(zoneName);
+            }
+            Collections.sort(zoneNames);
+            System.out.println("Alphabetically sorted list of geo zones for " + countryIndex + " is this: " + zoneNames);
+        } else {
+            assertTrue(openPage("Countries"));
+        }
+    }
+
+    public void loginToAdminPageAsAdmin() {
         driver.get("http://localhost:8090/litecart/admin/");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
@@ -65,12 +117,19 @@ public class Task8 extends CommonMethods{
         return found;
     }
 
-    public boolean openPage(String pageName){
-        if (isElementPresent("//span[@class='name'][contains(.,'" + pageName + "')]")){
-           driver.findElement(By.xpath("//span[@class='name'][contains(.,'" + pageName + "')]")).click();
-           return true;
-        }
-        else{
+    public boolean areElementsPresent(String xpath) {
+        boolean found;
+        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        found = driver.findElements(By.xpath(xpath)).size() > 0;
+        driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+        return found;
+    }
+
+    public boolean openPage(String pageName) {
+        if (isElementPresent("//span[@class='name'][contains(.,'" + pageName + "')]")) {
+            driver.findElement(By.xpath("//span[@class='name'][contains(.,'" + pageName + "')]")).click();
+            return true;
+        } else {
             System.out.println("Page not found");
             driver.quit();
             return false;
