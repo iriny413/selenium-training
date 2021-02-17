2b3d6fc3d84dc2dd16588da246963ef5cbc6c007
ikhvichi <irina.khvichiya@t-systems.com>
2021-02-17 08:39:13 +0100

    task 11 - update

--- java-example/src/test/java/ru/selenium/training/Task11.java
+++ java-example/src/test/java/ru/selenium/training/Task11.java
@@ -35,8 +35,8 @@ public class Task11 extends CommonMethods {
         WebElement s1_e3 = driver.findElement(s1_elem3);
         WebElement s1_e4 = driver.findElement(s1_elem4);
         WebElement s2_e2 = driver.findElement(s2_elem2);
-        new Actions(driver).moveToElement(s1_e2).doubleClick(s1_e2).dragAndDrop(s1_e2, s1_e4).perform();
-        new Actions(driver).moveToElement(s1_e3).doubleClick(s1_e3).dragAndDrop(s1_e3, s2_e2).perform();
+        new Actions(driver).moveToElement(s1_e2).click(s1_e2).dragAndDrop(s1_e2, s1_e4).perform();
+        new Actions(driver).moveToElement(s1_e3).click(s1_e3).dragAndDrop(s1_e3, s2_e2).perform();
     }
 
     public void login() {
